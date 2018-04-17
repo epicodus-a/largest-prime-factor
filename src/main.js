0f125e8c02020471426b0a9c2d7eebd49abe0a43
Adriana C and Brea Borlas <adriana@>
2018-04-17 13:49:00 -0700

    Add form and front end jquery

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import{UserInput} from "./largest-prime-factor";
+
+$().ready(function(){
+  $(".largest-prime").submit(function(e){
+    e.preventDefault();
+    let number  = $("#int").val();
+    let input = new UserInput(number);
+    let output = input.getLargestPrime();
+
+    $(".output").html(output);
+  })
+})
