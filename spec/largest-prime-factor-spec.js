39aa5af3d0d8c85ce6bd0cf67f9dec11dc5531f7
Adriana C and Brea Borlas <adriana@>
2018-04-17 11:17:15 -0700

    Add test for UserInput constructor

--- spec/largest-prime-factor-spec.js
+++ spec/largest-prime-factor-spec.js
@@ -1,7 +1,15 @@
-import{factor} from "../src/largest-prime-factor";
+import {factor} from "../src/largest-prime-factor";
+import {UserInput} from "../src/largest-prime-factor";
 
 describe("factor", function(){
   it("should return true", function(){
     expect(factor()).toBeTruthy();
-  })
-})
+  });
+});
+
+describe("UserInput", function(){
+  it("should store a number as a property when constructed", function(){
+    let newUserInput = new UserInput(4);
+    expect(newUserInput.number).toEqual(4);
+  });
+});
--- src/largest-prime-factor.js
+++ src/largest-prime-factor.js
@@ -9,4 +9,24 @@ export class UserInput{
     this.factors = [];
   }
 
+  // getFactors (){
+  //   for (let i=1; i<=Math.sqrt(this.number); i++){
+  //     if (this.number % i === 0){
+  //       this.factors.push(i);
+  //     }
+  //   }
+  // }
+  //
+  // isPrime (possPrime) {
+  //   for (let j=2; j<=Math.sqrt(possPrime); j++){
+  //     if (j !== possPrime && possPrime%j === 0){
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+
 }
+
+let testUserInput = new UserInput(4);
+testUserInput.number;
