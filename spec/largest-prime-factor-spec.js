c05dc30438bba0499186b42c84cf8299f18d9eb2
Adriana C and Brea Borlas <adriana@>
2018-04-17 13:37:25 -0700

    add test for getLargestPrime

--- spec/largest-prime-factor-spec.js
+++ spec/largest-prime-factor-spec.js
@@ -18,8 +18,25 @@ describe("UserInput", function(){
     expect(reusableUserInput.number).toEqual(26);
   });
 
-  it("should store an array of factors when getFactors is called upon the UserInput instance", function(){
+  it("should store an array when getFactors is called upon the UserInput instance", function(){
     reusableUserInput.getFactors();
     expect(reusableUserInput.factors instanceof Array).toBeTruthy();
   });
+
+  it("should store an array of factors when getFactors is called upon the UserInput instance", function(){
+    reusableUserInput.getFactors();
+    expect(reusableUserInput.factors).toContain(13);
+    expect(reusableUserInput.factors).toContain(2);
+    expect(reusableUserInput.factors).toContain(1);
+  });
+
+  it("should return true for 5", function(){
+    expect(reusableUserInput.isPrime(5)).toBeTruthy();
+  });
+
+  it("should return the largest prime when getLargestPrime get called", function(){
+    let input = new UserInput(10);
+    expect(input.getLargestPrime()).toEqual(5);
+  })
+
 });
--- src/largest-prime-factor.js
+++ src/largest-prime-factor.js
@@ -10,23 +10,36 @@ export class UserInput{
   }
 
   getFactors (){
-    for (let i=1; i<=Math.sqrt(this.number); i++){
+    for (let i=1; i<=this.number; i++){
       if (this.number % i === 0){
         this.factors.push(i);
       }
     }
+    return this.factors;
   }
 
-  // isPrime (possPrime) {
-  //   for (let j=2; j<=Math.sqrt(possPrime); j++){
-  //     if (j !== possPrime && possPrime%j === 0){
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // }
+  isPrime (possPrime) {
+    for (let j=2; j<=Math.sqrt(possPrime); j++){
+      if (j !== possPrime && possPrime%j === 0){
+        return false;
+      }
+    }
+    return true;
+  }
 
+  getLargestPrime() {
+    let arr = this.getFactors();
+    let largestPrime = 1;
+    // arr.forEach((element) => {
+    //   if (this.isPrime(element) && element > largestPrime){
+    //     largestPrime = element;
+    //   }
+    // });
+    for(let i=0; i<arr.length; i++){
+      if (this.isPrime(arr[i]) && arr[i] > largestPrime){
+        largestPrime = arr[i];
+      }
+    }
+    return largestPrime;
+  }
 }
-
-let testUserInput = new UserInput(4);
-testUserInput.number;
